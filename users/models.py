5fcb6763428ce09d5b789177ea77c293fc89f43d
datapoint456743-svg <datapoint456743@gmail.com>
2026-03-15 15:43:21 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,21 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
 class UserRegistrationModel(models.Model):
+
     name = models.CharField(max_length=100)
-    loginid = models.CharField(unique=True, max_length=100)
-    password = models.CharField(max_length=100)
-    mobile = models.CharField(unique=True, max_length=100)
-    email = models.CharField(unique=True, max_length=100)
-    locality = models.CharField(max_length=100)
-    address = models.CharField(max_length=1000)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
+
+    loginid = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    password = models.CharField(
+        max_length=100
+    )
+
+    mobile = models.CharField(
+        max_length=10,
+        unique=True
+    )
+
+    email = models.EmailField(
+        max_length=100,
+        unique=True
+    )
+
+    locality = models.CharField(
+        max_length=100
+    )
+
+    address = models.TextField(
+        max_length=1000
+    )
+
+    city = models.CharField(
+        max_length=100
+    )
+
+    state = models.CharField(
+        max_length=100
+    )
+
+    status = models.CharField(
+        max_length=100,
+        default='waiting'
+    )
 
     def __str__(self):
         return self.loginid
 
+
     class Meta:
         db_table = 'UserRegistrations'
-
