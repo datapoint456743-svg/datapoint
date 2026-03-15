4e2da78f8d9e81674bfb0485835891398051a89b
datapoint456743-svg <datapoint456743@gmail.com>
2026-03-15 15:38:33 +0530

    Update views.py

--- Automatic_English_Essay_Scoring_Algorithm_Based_On_Ml/views.py
+++ Automatic_English_Essay_Scoring_Algorithm_Based_On_Ml/views.py
@@ -1,17 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from users.forms import UserRegistrationForm
 
 
+# ---------------- HOME PAGE ---------------- #
+
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html')
+
+
+# ---------------- ADMIN LOGIN PAGE ---------------- #
 
 def AdminLogin(request):
-    return render(request, 'AdminLogin.html', {})
+    return render(request, 'AdminLogin.html')
+
+
+# ---------------- USER LOGIN PAGE ---------------- #
 
 def UserLogin(request):
-    return render(request, 'UserLogin.html', {})
+    return render(request, 'UserLogin.html')
 
+
+# ---------------- USER REGISTRATION ---------------- #
 
 def UserRegister(request):
-    form = UserRegistrationForm()
-    return render(request, 'UserRegistrations.html', {'form': form})
+
+    if request.method == "POST":
+
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Redirect to login page after registration
+            return redirect('UserLogin')
+
+    else:
+        form = UserRegistrationForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'UserRegistrations.html', context)
